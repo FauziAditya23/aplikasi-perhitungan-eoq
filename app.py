f92733d436807e0e48677a73ea2bcff5be418cda
Fauzi Aditya <fauziaditya874@jmail.com>
2025-07-10 16:45:38 +0700

    add main program

--- app.py
+++ app.py
@@ -77,8 +77,8 @@ st.divider() # Garis pemisah visual
 D_default = 150000
 S_default = 7500000 
 H_default = 25000 
-lead_time_default = 10 # Lead Time Pengiriman (hari) - Tetap
-safety_stock_default = 500 # Stok Pengaman (kg) - Lebih realistis
+lead_time_default = 7 # Lead Time Pengiriman (hari) - Tetap
+safety_stock_default = 300 # Stok Pengaman (kg) - Lebih realistis
 
 # --- Kolom Panduan Aplikasi (di Sidebar) ---
 with st.sidebar:
